40019d8928f1452f43ab7aff30240e0598acf792
Mavludin <imevlud@gmail.com>
2023-09-27 20:36:30 +0300

    check different outputs

--- assets/js/w3c-validator.mjs
+++ assets/js/w3c-validator.mjs
@@ -10,10 +10,18 @@
 import { w3cHtmlValidator } from 'w3c-html-validator';
 
 // Formatted output
-const options = { continueOnFail: true, maxMessageLen: 80, output: 'json' };
+const options = { continueOnFail: true, maxMessageLen: 80 };
 const customReporter = (results) => w3cHtmlValidator.reporter(results, options);
-w3cHtmlValidator
-  .validate({
-    filename: '/home/runner/work/intense-it-page/intense-it-page/index.html',
-  })
-  .then(customReporter);
+w3cHtmlValidator.validate({ filename:  '/home/runner/work/intense-it-page/intense-it-page/index.html' }).then(w3cHtmlValidator.reporter);
+console.log('///////////////////////');
+w3cHtmlValidator.validate({ filename: '/home/runner/work/intense-it-page/intense-it-page/index.html' }).then(w3cHtmlValidator.reporter);
+console.log('///////////////////////');
+w3cHtmlValidator.validate({ filename: '/home/runner/work/intense-it-page/intense-it-page/index.html' }).then(customReporter);
+
+console.log('///////////////////////');
+
+
+// JSON output
+const sleep = (data) => new Promise(resolve => setTimeout(() => resolve(data), 2000));
+const log =   (results) => console.log('\nValidatorResults:', results);
+w3cHtmlValidator.validate({ filename: '/home/runner/work/intense-it-page/intense-it-page/index.html' }).then(sleep).then(log);
