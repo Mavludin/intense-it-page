8edbc4b53fe11c0be31acf5aca6a026effb5a49b
Mavludin <imevlud@gmail.com>
2023-09-27 20:16:35 +0300

    run w3c-validator locally

---
+++ assets/js/w3c-validator.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+////////////////////////
+// W3C HTML Validator //
+// Examples           //
+////////////////////////
+
+// Command to run:
+//    $ node examples.js
+
+import { w3cHtmlValidator } from 'w3c-html-validator';
+
+// Formatted output
+const options = { continueOnFail: true, maxMessageLen: 80, output: 'html' };
+const customReporter = (results) => w3cHtmlValidator.reporter(results, options);
+w3cHtmlValidator.validate({ filename: '../../index.html' }).then(customReporter);
