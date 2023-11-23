250f403935bf6a298b6d1fd7300290c0a0505c55
Mavludin <imevlud@gmail.com>
2023-11-23 19:33:29 +0300

    edit author and description meta info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
-/*!
-=========================================================
-* Ollie Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
 // smooth scroll
 $(document).ready(function () {
 
